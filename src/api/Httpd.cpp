1982306b960437999ce757be291dc0a89a03f7f1
OfSize <36066037+OfSize@users.noreply.github.com>
2018-03-01 15:41:17 +0300

    Update Httpd.cpp

--- src/api/Httpd.cpp
+++ src/api/Httpd.cpp
@@ -8,7 +8,7 @@ Httpd::Httpd(int port, const char *accessToken) :
     m_port(port),
     m_daemon(nullptr)
 {}
-bool Httpd::start(){}
-int Httpd::auth(const char *header){}
-int Httpd::done(MHD_Connection *connection, int status, MHD_Response *rsp){}
-int Httpd::handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls){}
+bool Httpd::start(){return 0;}
+int Httpd::auth(const char *header){return 0;}
+int Httpd::done(MHD_Connection *connection, int status, MHD_Response *rsp){return 0;}
+int Httpd::handler(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *upload_data, size_t *upload_data_size, void **con_cls){return 0;}
